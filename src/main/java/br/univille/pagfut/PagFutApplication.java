23fad43b385f2fe9f713bec37a8cd97291628c27
GUILHERME PASSOS DE BORBA <guilhermepassos@univille.br>
2025-08-28 21:28:59 -0300

    first commit

---
+++ src/main/java/br/univille/pagfut/PagFutApplication.java
@@ -0,0 +1,13 @@
+package br.univille.pagfut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagFutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagFutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/univille/pagfut/PagFutApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.pagfut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagFutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
